a86c0150fd2f11b7ded8dbf125068fa4ceac0757
yanisLenovoMaman <yanistabellout4@gmail.com>
2022-05-07 21:51:15 +0100

    added little triangle

--- Components/About.php
+++ Components/About.php
@@ -148,4 +148,5 @@
             </div>
         </div>
     </div>
+    <div class="triangle"id='triangle'></div>
 </div>
--- JS/messenger.js
+++ JS/messenger.js
@@ -2,11 +2,14 @@ let microDisplay=document.getElementById('micro-display');
 let aboutPage=document.getElementById('about');
 let microLogoMesg=microDisplay.firstElementChild;
 let closeMessenger=document.getElementById('close-messenger');
+let triangle=document.getElementById('triangle');
+
 const modalOverlayMessenger=document.getElementById('overlay-messenger');
 microDisplay.addEventListener('click',(e)=>{
 
    aboutPage.classList.toggle('show');
    microDisplay.classList.toggle('transition');
+   triangle.classList.toggle('show');
    if(aboutPage.classList.contains('show')) modalOverlayMessenger.classList.add('active');
    else modalOverlayMessenger.classList.remove('active');
    if(microLogoMesg.getAttribute('data-before')==1)microLogoMesg.setAttribute('data-before',0);
